053c059ee8ca1bd4f6ae3ff858a2add88a3cf563
Joe Kurleto <joekur@reverb.com>
2017-05-16 22:28:25 -0500

    Fix call to static_path

--- web/view_helpers.ex
+++ web/view_helpers.ex
@@ -1,9 +1,11 @@
 defmodule SneakyChat.ViewHelpers do
+  import SneakyChat.Router.Helpers
+
   def webpack_path(conn, path) do
     if Mix.env == :dev do
       "http://localhost:8080/priv/static/js/#{path}"
     else
-      Phoenix.Router.Helpers.static_path(conn, "js/#{path}")
+      static_path(conn, "/js/#{path}")
     end
   end
 end
